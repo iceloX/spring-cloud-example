37a2e509c10aad7fc9d1735a62c0209f1372e844
icelox <2568450165@qq.com>
2021-12-21 10:12:24 +0800

    '熔断器'

--- springcloud_parent/springcloud_10_hystrix_dashboard/src/main/java/cn/icelo/HystrixDashboardApplication.java
+++ springcloud_parent/springcloud_10_hystrix_dashboard/src/main/java/cn/icelo/HystrixDashboardApplication.java
@@ -1,5 +1,29 @@
 package cn.icelo;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient // 注意：默认只要引入Hystrix来就表明注册上了，无需显示声明
+@EnableHystrixDashboard  // 这个注解作用，用来开启当前应用为仪表盘服务
 public class HystrixDashboardApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
